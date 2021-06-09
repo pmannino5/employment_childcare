9071476cd31e80086aa6a44bcb084aafa2c4c718
pmannino5 <43046160+pmannino5@users.noreply.github.com>
2021-06-08 19:00:30 -0700

    Update pandemic_employment.R

--- pandemic_employment.R
+++ pandemic_employment.R
@@ -1,12 +1,20 @@
-# This script replicates and extends Jason Furman, Melissa Kearney, and Wilson Powell's analysis of childcare and employment - https://www.piie.com/blogs/realtime-economic-issues-watch/how-much-have-childcare-challenges-slowed-us-jobs-market
+# This script replicates and extends Jason Furman, Melissa Kearney, and Wilson Powell's analysis of childcare and employment - 
+# https://www.piie.com/blogs/realtime-economic-issues-watch/how-much-have-childcare-challenges-slowed-us-jobs-market
 
-# They calculate the % change in employment to population ratio between Q1-2020 and Q1-2021 for men and women with and without a bachelors degree and with at least 1 child less than 13 years old (who needs childcare) compared to all other adults. They then do a counterfactual analysis that asks how the total YOY employment rate for men and women would change if parents with kids less than 13 had the employment rates of all other adults.
+# They calculate the % change in employment to population ratio between Q1-2020 and Q1-2021 for men and women with and without 
+# a bachelors degree and with at least 1 child less than 13 years old (who needs childcare) compared to all other adults. 
+# They then do a counterfactual analysis that asks how the total YOY employment rate for men and women would change if parents 
+# with kids less than 13 had the employment rates of all other adults.
 
-# One potential shortcoming here is that all other adults might not be the right counterfactual, but maybe parents of kids 13 and over would be a better counterfactual as we are still looking at parents, but parents that don't have to provide childcare.
+# One potential shortcoming here is that all other adults might not be the right counterfactual, but maybe parents of kids 13 and over 
+# would be a better counterfactual as we are still looking at parents, but parents that don't have to provide childcare.
 
-# This script replicates their first graph and then shows how it would be different if instead of using all adults as a comparison, we use parents with kids over 13 as a comparison.
+# This script replicates their first graph and then shows how it would be different if instead of using all adults as a comparison, 
+# we use parents with kids over 13 as a comparison.
 
-# It then replicates their counterfactual analysis in table 1 for employment-population ratio changes. The outcome doesn't EXACTLY match their results, but it's very close. I then redo the counterfactual, but use the employment-population ratio changes for parents with kids over 13 as the counterfactual. 
+# It then replicates their counterfactual analysis in table 1 for employment-population ratio changes. 
+# The outcome doesn't EXACTLY match their results, but it's very close. I then redo the counterfactual, 
+# but use the employment-population ratio changes for parents with kids over 13 as the counterfactual. 
 
 # Need to clean and document code better - still pretty stream of consciousness coding
 
@@ -29,6 +37,7 @@ if (!require("ipumsr")) stop("Reading IPUMS data into R requires the ipumsr pack
 ddi <- read_ipums_ddi("cps_00001.xml")
 data <- read_ipums_micro(ddi)
 
+# recode and filter
 epop <- data %>% 
   mutate(young_child = if_else(YNGCH<13,'<13',
                              if_else(YNGCH == 99, 'no_children', '>=13')),
@@ -110,7 +119,8 @@ epop %>% filter(AGE > 25 & AGE <= 64) %>%
 
 ### Replicate the first table using parents of young kids and all other adults
 
-# The authors create age-sex-education cells and then calculate how different the employment-population ratio would be if parents of young kids had the same change in employment rates as all other adults within each cell.
+# The authors create age-sex-education cells and then calculate how different the employment-population ratio would be if 
+# parents of young kids had the same change in employment rates as all other adults within each cell.
 
 # actual epop rate changes for men and women
 actual <- epop %>% filter(MONTH %in% c(1,2,3) & !is.na(age_bucket)) %>%
@@ -156,13 +166,21 @@ replicate_results_table <- actual %>%
   gt() %>% fmt_percent(columns = c(`Actual Percent Change`,`Counterfactual Percent Change`), decimals = 3) %>%
   tab_options(table.font.size = 8) %>% tab_header(title="Counterfactual Replication Results")
 
-gt:: gtsave(data=replicate_results_table, filename = 'replication_results.png')
+gtsave(data=replicate_results_table, filename = 'replication_results.png')
 
-# The results of this analysis match very closely, though not exactly with what FKP reported in their analysis. Basically, childcare seems to have very little impact on changes in epop rates because the difference in employment rates between parents of young kids and everyone else is pretty small and because the population of employed parents with young kids is pretty small, so assuming higher employment rates on this group has little impact on total epop changes of men and women.
+# The results of this analysis match very closely, though not exactly with what FKP reported in their analysis. 
+# Basically, childcare seems to have very little impact on changes in epop rates because the difference in employment 
+# rates between parents of young kids and everyone else is pretty small and because the population of employed parents 
+# with young kids is pretty small, so assuming higher employment rates on this group has little impact on total epop changes of men and women.
 
 ### A revised counterfactual using parents of older kids
 
-# This does the same thing as above, but breaks out adults into parents of young kids, older kids, and no kids and assumes the counterfactual where the parents of young kids have the same YOY epop ratio change as the parents of old kids (instead of all othe adults). The problem now is that some of the cells are pretty small. In particular the 16-24 year old cell with kids older than 13 is really small (a 16 year old can't have a kid over 13 years old), so epop changes are huge (100% decrease in one case) and does not reflect reality. So for this analysis I remove the 16-24 age group, and then run the revised counterfactual exercise. 
+# This does the same thing as above, but breaks out adults into parents of young kids, older kids, 
+# and no kids and assumes the counterfactual where the parents of young kids have the same YOY epop ratio 
+# change as the parents of old kids (instead of all othe adults). The problem now is that some of the cells are pretty small. 
+# In particular the 16-24 year old cell with kids older than 13 is really small (a 16 year old can't have a kid over 13 years old), 
+# so epop changes are huge (100% decrease in one case) and does not reflect reality. So for this analysis I remove the 16-24 age group, 
+# and then run the revised counterfactual exercise. 
 
 # actual epop rate changes for men and women excluding 16-24 year olds
 actual_revise <- epop %>% filter(MONTH %in% c(1,2,3) & !is.na(age_bucket)  & age_bucket != '16-24') %>%
@@ -209,4 +227,4 @@ revised_replicate_results_table <- actual_revise %>%
   gt() %>% fmt_percent(columns = c(`Actual Percent Change`,`Counterfactual Percent Change`), decimals = 3) %>%
   tab_options(table.font.size = 8) %>% tab_header(title="Revised Counterfactual Replication Results")
 
-gt:: gtsave(data=revised_replicate_results_table, filename = 'revised_replication_results.png')
+gtsave(data=revised_replicate_results_table, filename = 'revised_replication_results.png')
